72310dc5ec2ca84b9650f123d8fe4c36725182ab
roslofe1 <ella.roslof@aalto.fi>
2022-12-01 13:41:30 +0200

    Added successful implementation of the second half of the day 1 exercise.

--- src/main/scala/day1/calorieCounter.scala
+++ src/main/scala/day1/calorieCounter.scala
@@ -4,7 +4,8 @@ import scala.math.max
 
 /**The purpose of the object is to find the elf carrying the largest amount of calories, and return said calorie amount. For more information, see: https://adventofcode.com/2022/day/1*/
 object calorieCounter extends App:
-  private def findCals() =
+  //FindCals can find either the maximum individual calories, or the sum of the top three, as requested in the second half. This is determined by the top3 variable
+  private def findCals(top3: Boolean) =
     //Initialising variables to read the file
     try
       val file = File("./src/main/scala/day1/data.txt")
@@ -12,20 +13,36 @@ object calorieCounter extends App:
       val lineReader = BufferedReader(fileReader)
       //Attempt to read file data
       try
-        //Store the max number of calories
+        //Store the max number of calories, or array of top 3. The one being used depends on the top3 variable
         var currentMax = 0
+        var top = Array.fill(3)(0)
         var line = lineReader.readLine()
         var currentCals = 0
         while line != null do
           //If all the items from the elf have been processed, update the max if needed, and set the counter to 0
           if line == "" then
-            currentMax = max(currentMax, currentCals)
+            if top3 then
+              //Check if amount is larger than one of the ones in the array. If so, update the appropriate index
+              val maxComparison = top.map(x => x.compare(currentCals))
+              val replacingIndex = maxComparison.indexWhere(_ == -1)
+              //Change the rest of the top if needed
+              var i = 2
+              while i > replacingIndex && replacingIndex != -1 do
+                top.update(i, top(i - 1))
+                i -= 1
+              if replacingIndex != -1 then
+                top.update(replacingIndex, currentCals)
+            else
+              currentMax = max(currentMax, currentCals)
             currentCals = 0
           //Otherwise add the read amount to the counter
           else
             currentCals += line.toInt
           line = lineReader.readLine()
         //Return the max amount of calories
+        if top3 then
+          top.sum
+        else
         currentMax
       finally
         fileReader.close()
@@ -35,7 +52,8 @@ object calorieCounter extends App:
       case _ => println("Problems with reading the file")
   end findCals
 
-  println(s"Found maximum: ${findCals()}")
+  println(s"Found individual maximum: ${findCals(false)}")
+  println(s"Found group maximum: ${findCals(true)}")
 
 end calorieCounter
 
