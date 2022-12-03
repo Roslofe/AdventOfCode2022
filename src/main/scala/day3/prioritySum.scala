ad6d25aeb541e29eda988b31d40ea90cf32ce1a7
roslofe1 <ella.roslof@aalto.fi>
2022-12-03 21:35:33 +0200

    Added successful implementation of the second half of the day 3 exercise.

--- src/main/scala/day3/prioritySum.scala
+++ src/main/scala/day3/prioritySum.scala
@@ -13,10 +13,13 @@ object prioritySum extends App:
    * @return    The sum of the priorities of the common values
    * */
   private def compareItems(a: Array[Char], b: Array[Char]) = a.intersect(b).distinct.map(x => priorityValues.indexOf(x) + 1).sum
+  /** compare3 does the same thing as compareItems, but with three inputs. */
+  private def compare3(a: Array[Char], b: Array[Char], c: Array[Char]) = a.intersect(b).intersect(c).distinct.map(x => priorityValues.indexOf(x) + 1).sum
 
   /** findPriority reads the files, as well as calls the other functions.
-   * @return  The sum of all the common inputs */
-  private def findPriority() =
+   * @param first   Determines whether the first or second half of the exercise is being evaluated.
+   * @return        The sum of all the common inputs */
+  private def findPriority(first: Boolean) =
     try
       val file = File("./src/main/scala/day3/data.txt")
       val fileReader = FileReader(file)
@@ -25,12 +28,26 @@ object prioritySum extends App:
       try
         var line = lineReader.readLine()
         var currScore = 0
+        /** Stores the three lines used in each evaluation. */
+        val elfGroup = Array.fill(3)(Array('a'))
+        var elfIndex = 0
         while line != null do
           val individuals = line.toCharArray
-          //Determine size of each half
-          val separatingSize = individuals.length / 2
-          currScore += compareItems(individuals.take(separatingSize), individuals.takeRight(separatingSize))
+          if first then
+            //Determine size of each half
+            val separatingSize = individuals.length / 2
+            currScore += compareItems(individuals.take(separatingSize), individuals.takeRight(separatingSize))
+          else if elfIndex < 3 then
+            //Add the current Array to elfGroup for later evaluation
+            elfGroup.update(elfIndex, individuals)
+            elfIndex += 1
+          else
+            //Evaluate the Arrays in elfGroup to get the score
+            currScore += compare3(elfGroup(0), elfGroup(1), elfGroup(2))
+            elfGroup.update(0, individuals)
+            elfIndex = 1
           line = lineReader.readLine()
+        currScore += compare3(elfGroup(0), elfGroup(1), elfGroup(2))
         currScore
       finally
         fileReader.close()
@@ -40,7 +57,8 @@ object prioritySum extends App:
       case _ => println("Problems with reading the file")
   end findPriority
 
-  println(s"Sum of duplicate items found: ${findPriority()}")
+  println(s"Sum of duplicate items found: ${findPriority(true)}")
+  println(s"Sum of group identifiers: ${findPriority(false)}")
 
 
 end prioritySum
