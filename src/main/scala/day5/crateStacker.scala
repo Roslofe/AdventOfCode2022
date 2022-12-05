934c191792af5b48cdeeb7a9554758914ab13508
roslofe1 <ella.roslof@aalto.fi>
2022-12-05 15:17:53 +0200

    Added successful implementation of the second half of the day 5 exercise.

--- src/main/scala/day5/crateStacker.scala
+++ src/main/scala/day5/crateStacker.scala
@@ -6,8 +6,9 @@ import java.io._
 object crateStacker extends App:
 
   /** moveCrates initialises, moves, and displays the results of the move.
-   * @return  The top crates of each stack. */
-  private def moveCrates() =
+   * @param first   Determines which half of the exercise is being executed.
+   * @return        The top crates of each stack. */
+  private def moveCrates(first: Boolean) =
     try
       val file = File("./src/main/scala/day5/data.txt")
       val fileReader = FileReader(file)
@@ -28,25 +29,35 @@ object crateStacker extends App:
                 crateStacks(crate._2).append(trimmed(1))
           line = lineReader.readLine()
         line = lineReader.readLine()
-        //Add the order of moving the crates
-        //From, to
-        val moveOrder = scala.collection.mutable.Queue[(Int, Int)]()
-        while line != null do
-          val instructions = line.split(" ")
-          var i = 0
-          while i < instructions(1).toInt do
-            moveOrder.append((instructions(3).toInt - 1, instructions(5).toInt - 1))
-            i += 1
-          line = lineReader.readLine()
-        //Finally, move the crates
-        while moveOrder.nonEmpty do
-          val currentMove = moveOrder.dequeue()
-          val fromStack = crateStacks(currentMove._1)
-          val moving = fromStack(0)
-          fromStack.remove(0)
-          crateStacks(currentMove._2).prepend(moving)
+        //For examining the first section of the exercise
+        if first then
+          //Add the order of moving the crates
+          //From, to
+          val moveOrder = scala.collection.mutable.Queue[(Int, Int)]()
+          while line != null do
+            val instructions = line.split(" ")
+            var i = 0
+            while i < instructions(1).toInt do
+              moveOrder.append((instructions(3).toInt - 1, instructions(5).toInt - 1))
+              i += 1
+            line = lineReader.readLine()
+          //Finally, move the crates
+          while moveOrder.nonEmpty do
+            val currentMove = moveOrder.dequeue()
+            val fromStack = crateStacks(currentMove._1)
+            val moving = fromStack(0)
+            fromStack.remove(0)
+            crateStacks(currentMove._2).prepend(moving)
+        //Second half of the exercise, moving multiple stacks at a time
+        else
+          while line != null do
+            val instructions = line.split(" ")
+            val movingStack = crateStacks(instructions(3).toInt - 1)
+            val moving = movingStack.take(instructions(1).toInt)
+            movingStack.remove(0, instructions(1).toInt)
+            crateStacks(instructions(5).toInt - 1).prependAll(moving)
+            line = lineReader.readLine()
         crateStacks.map(_(0)).mkString
-
       finally
         fileReader.close()
         lineReader.close()
@@ -55,6 +66,8 @@ object crateStacker extends App:
       case _ => println("Problems with reading the file")
   end moveCrates
 
-  println(s"Final top crates: ${moveCrates()}")
+  println(s"Final top crates: ${moveCrates(true)}")
+  println(s"Final top crates, when moving multiple simultaneously: ${moveCrates(false)}")
+
 end crateStacker
 
