581355d3d9f7b8e2fbbc91e931d882d6e6e1d30a
roslofe1 <ella.roslof@aalto.fi>
2022-12-06 18:30:30 +0200

    Added successful implementation of the second half of the day 6 exercise.

--- src/main/scala/day6/markerCheck.scala
+++ src/main/scala/day6/markerCheck.scala
@@ -1,14 +1,15 @@
 package day6
 import java.io._
 
-/** markerCheck reads an input stream, and detects when a four-character marker with distinct members is processed.
+/** markerCheck reads an input stream, and detects when a four/fourteen-character marker with distinct members is processed.
  * For more information, see: https://adventofcode.com/2022/day/6 */
 object markerCheck extends App:
 
-  /** Evaluates the stream, and ends processing once a four-character marker has been found.
-   * @return  The number of characters needed to process prior to finding a marker
+  /** Evaluates the stream, and ends processing once a four/fourteen-character marker has been found.
+   * @param first   Parameter determining which half of the exercise is being evaluated
+   * @return        The number of characters needed to process prior to finding a marker
    */
-  private def findMarker() =
+  private def findMarker(first: Boolean) =
     try
       val file = File("./src/main/scala/day6/data.txt")
       val fileReader = FileReader(file)
@@ -18,6 +19,8 @@ object markerCheck extends App:
         //Store the marker, as well as the number of markers processed
         var markerNum = -1
         var markersProcessed = 0
+        //Which number of characters is being evaluated, depends on the half of the exercise
+        val markerLenght = if first then 4 else 14
         //Characters that could be part of a marker
         val prevChars = scala.collection.mutable.ListBuffer[Char]()
         var currentChar = dataReader.read()
@@ -25,10 +28,10 @@ object markerCheck extends App:
           val charValue = currentChar.toChar
           markersProcessed += 1
           //Update the marker number if the current sequence is a marker
-          if !prevChars.contains(charValue) && prevChars.distinct.size == prevChars.size && markersProcessed > 4 then
+          if !prevChars.contains(charValue) && prevChars.distinct.size == prevChars.size && markersProcessed >= markerLenght then
             markerNum = markersProcessed
           else
-            if prevChars.size == 3 then
+            if prevChars.size == markerLenght - 1 then
               prevChars.remove(0)
             prevChars.append(charValue)
           currentChar = dataReader.read()
@@ -41,7 +44,8 @@ object markerCheck extends App:
       case _ => println("Problems with reading the file")
   end findMarker
 
-  println(s"First marker found after character: ${findMarker()}")
+  println(s"First four-character marker found after character: ${findMarker(true)}")
+  println(s"First fourteen-character marker found after character: ${findMarker(false)}")
 
 end markerCheck
 
